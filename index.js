fc2ed27500aa33262fb83bcc2bcb873168414339
Juan Manuel Martínez <jmartinez@e-bitware.com>
2019-10-06 13:52:03 -0500

    AutoPause w intersectionObserver & visibilityChange

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 import MediaPlayer from './MediaPlayer.js';
 import AutoPlay from './plugins/AutoPlay.js';
+import AutoPause from './plugins/AutoPause.js'
 
 const video = document.querySelector('video');
 const player = new MediaPlayer({
   el: video,
   plugins: [
-    new AutoPlay()
+    new AutoPlay(),
+    new AutoPause()
   ],
 });
 
@@ -21,4 +23,8 @@ muteButton.onclick = () => {
   }
 };
 
-
+if('serviceWorker' in navigator){
+  navigator.serviceWorker.register('/sw.js').catch(error => {
+    console.log(error.message)
+  })
+}
--- plugins/AutoPlay.js
+++ plugins/AutoPlay.js
@@ -1,7 +1,5 @@
 function AutoPlay() {}
 AutoPlay.prototype.run = function(player) {
-  console.log(player.muted)
-  console.log('se ejecuta este pedo')
   if (!player.muted) {
     player.muted = true;
   }
